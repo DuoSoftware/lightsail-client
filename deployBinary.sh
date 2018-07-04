fc03c224a2d50f67436e63df16094ace651e3327
Prasad Jayashanka <prasadjayashanka@gmail.com>
2018-07-04 13:40:18 +0530

    changed deploy script

--- deployBinary.sh
+++ deployBinary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo rm $1
-sudo wget http://$2.smoothflow.io/engine/$1
+sudo wget http://$2.smoothflow.io/engine/BuiltExecutables/$1/$1
 sudo chmod 777 $1
 sudo nohup ./$1 > /dev/null 2>&1 &
