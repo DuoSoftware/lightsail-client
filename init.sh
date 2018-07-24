6b7b43f7234669031d45b091bacb64d36aa2facf
KalanaDemel2 <kalana@duosoftware.com>
2018-07-24 11:50:32 +0530

    Update

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sudo npm install -g forever
 sudo export EDITOR=/usr/bin/vim.basic
-sudo crontab -l && echo "@reboot /usr/bin/forever start /lightsail-client/app.js" | crontab -
+sudo crontab < my.cron
+#sudo crontab -l && echo "@reboot /usr/bin/forever start /lightsail-client/app.js" | crontab -
