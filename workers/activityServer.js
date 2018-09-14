767aa3186545d59a0ce67f3a461f29b32b804a90
KalanaDemel2 <kalana@duosoftware.com>
2018-09-14 18:47:25 +0530

    Added response callback

--- workers/activityServer.js
+++ workers/activityServer.js
@@ -3,7 +3,7 @@ let shelljs = require('shelljs');
 
 module.exports.runWebServer = (res)=>{
 
-    child = shelljs.exec('bash deployWebSever.sh', {silent: false, async: true});
+    child = shelljs.exec('bash /lighsail-client/deployWebSever.sh', {silent: false, async: true});
 
     child.on('exit', function (c) {
         console.log(c);
