46af9baed8f55b77b2a97ce1e5656f77ac4913c6
KalanaDemel2 <kalana@duosoftware.com>
2018-09-21 14:55:20 +0530

    Updated for LightSail

--- deployWebServer.sh
+++ deployWebServer.sh
@@ -3,7 +3,7 @@ cd /
 cp -a smoothflow-activity-server smoothflow-activity-server-old
 rm -r smoothflow-activity-server
 git clone -b development https://duobuilduser:DuoS12345@github.com/DuoSoftware/smoothflow-activity-server.git
-cd smoothflow-activity-server
+cd /smoothflow-activity-server
 npm install
 forever stop app.js
 forever start app.js
