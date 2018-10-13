ac39dd3645f61955183832920d744d324e4c5d4e
KalanaDemel2 <vmkdemel@gmail.com>
2018-10-13 13:13:35 +0530

    Removed Forever

--- deployWebServer.sh
+++ deployWebServer.sh
@@ -5,6 +5,8 @@ rm -r smoothflow-activity-server
 git clone -b development https://duobuilduser:DuoS12345@github.com/DuoSoftware/smoothflow-activity-server.git
 cd /smoothflow-activity-server
 npm install
-forever stop app.js
-forever start app.js $1 $2
+OUTPUT="$(netstat -lpn | grep '5555' | cut -d 'N' -f 2 | tr -d '[:space:]'  | cut -d '/' -f 1)"
+echo "${OUTPUT}"
+kill -KILL  "${OUTPUT}"
+node app.js $1 $2
 rm -r /smoothflow-activity-server-old
